8ba8c123160265e111e4ac66d5ffd1dcead0db8e
DaniloCHGS <danilochagasbm@gmail.com>
2023-07-31 11:48:50 -0300

    Criando as actions e utilizando o useAppSelector tipado com os estados

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,10 +1,16 @@
 import { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store";
 
 export function AddTodo() {
   const [newTodo, setNewTodo] = useState<string>("");
+  const dispatch = useDispatch();
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
+
+    dispatch(add({ newTodo }));
+    setNewTodo("");
   }
 
   return (
